de1a0a2f94a05731e38eb201d2d4936f0ea08d88
robertkrimen <robertkrimen@gmail.com>
2010-04-28 23:12:12 -0700

    No more state!

--- lib/String/Cursor.pm
+++ lib/String/Cursor.pm
@@ -9,53 +9,35 @@ use Any::Moose;
 has data => qw/ is ro lazy_build 1 isa Str /;
 sub _build_data { '' }
 
-has state => qw/ is rw lazy_build 1 isa String::Cursor::State clearer clear_state /;
-sub _build_state { shift->__state };
-
-sub __state {
-    my $self = shift;
-    return String::Cursor::State->new( string => $self, position => 0, @_ );
-}
-sub _state {
-    my $self = shift;
-    $self->state( $self->__state( @_ ) );
-}
+has [qw/ head tail /] => qw/ is rw required 1 lazy 1 isa Int default 0 /;
 
 sub reset {
     my $self = shift;
-    $self->clear_state;
+    $self->head( 0 );
+    $self->tail( 0 );
     return $self;
 }
 
 sub move {
     my $self = shift;
-    return $self->state->move( @_ );
-}
-
-package String::Cursor::State;
-
-use Any::Moose;
-
-has string => qw/ is ro required 1 isa String::Cursor weak_ref 1 /, handles => [qw/ _state data /]; 
-has position => qw/ is ro required 1 isa Int /;
-
-sub move {
-    my $self = shift;
-    my $by = shift;
+    my $move = shift;
 
     my $data = $self->data;
 
-    if ( ref $by eq 'Regexp' ) {
-        pos $data = $self->position;
-        return unless scalar $data =~ m/\G.*?($by)/;
-        $position = $+[0];
-        return $self->_state( position => $position );
+    if ( ref $move eq 'Regexp' ) {
+        pos $data = $self->tail;
+        return unless scalar $data =~ m/\G.*?($move)/;
+        $self->head( $-[0] );
+        $self->tail( $+[0] );
+        return 1;
     }
-    elsif ( $by =~ m/^[\-\+]?\d+$/ ) {
-        my $position = $self->position + $by;
-        my $end = length $data;
-        $position = $end if $position > $end;
-        return $self->_state( position => $position );
+    elsif ( $move =~ m/^[\-\+]?\d+$/ ) {
+        my $position = $self->tail + $move;
+        my $length = length $data;
+        $position = $length if $position > $length;
+        $self->head( $position );
+        $self->tail( $position );
+        return 1;
     }
     
     die "Not ready";
